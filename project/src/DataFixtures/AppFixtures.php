01e5cdc0a38734fffaf08c420ecc02c495b5fda0
alex <o.lototskyi@gmail.com>
2023-08-18 15:52:07 +0300

    using fixtures

--- project/src/DataFixtures/AppFixtures.php
+++ project/src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\BlogPost;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,21 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A first Post!');
+        $blogPost->setPublished(new \DateTime('2023-08-18 12:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor('Alex');
+        $blogPost->setSlug('a-first-post');
+        $manager->persist($blogPost);
+
+        $blogPost = new BlogPost();
+        $blogPost->setTitle('A second Post!');
+        $blogPost->setPublished(new \DateTime('2023-08-18 13:00:00'));
+        $blogPost->setContent('Post text!');
+        $blogPost->setAuthor('Alex');
+        $blogPost->setSlug('a-second-post');
+        $manager->persist($blogPost);
 
         $manager->flush();
     }
