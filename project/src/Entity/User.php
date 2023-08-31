ae88c5d080362f4f05b64d04e37431e69a378aed
alex <o.lototskyi@gmail.com>
2023-08-31 19:51:16 +0300

    custom normalizer to show own email

--- project/src/Entity/User.php
+++ project/src/Entity/User.php
@@ -102,7 +102,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="string", length=255)
-     * @Groups({"post", "put", "get-admin"})
+     * @Groups({"post", "put", "get-admin", "get-owner"})
      * @Assert\NotBlank()
      * @Assert\Email()
      */
@@ -122,6 +122,7 @@ class User implements UserInterface
 
     /**
      * @ORM\Column(type="simple_array", length=200)
+     * @Groups({"get-admin", "get-owner"})
      */
     private $roles;
 
