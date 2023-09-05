c93449225f0bdf5fab489269d57e36fef5981612
alex <o.lototskyi@gmail.com>
2023-09-05 11:36:38 +0300

    user confirmation using custom entity and user confirmation subscriber

---
+++ project/src/Entity/UserConfirmation.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiProperty;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ApiResource(
+ *     collectionOperations={
+ *         "post"={
+ *              "path"="/users/confirm"
+ *         }
+ *     },
+ *     itemOperations={}
+ * )
+ *
+ */
+class UserConfirmation
+{
+
+    /**
+     * @ApiProperty(identifier=true)
+     * @Assert\NotBlank()
+     * @Assert\Length(min=30, max=30)
+     */
+    public $confirmationToken;
+}
