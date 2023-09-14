426235d228601f4cdcdea5dd74d93ded98d6daaa
alex <o.lototskyi@gmail.com>
2023-09-14 11:56:35 +0300

    Functional testing (BDD) using Behat

---
+++ project/features/bootstrap/FeatureContext.php
@@ -0,0 +1,99 @@
+<?php
+
+
+use App\DataFixtures\AppFixtures;
+use Behatch\Context\RestContext;
+use Behatch\HttpCall\Request;
+use Coduo\PHPMatcher\Factory\SimpleFactory;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+
+class FeatureContext extends RestContext
+{
+    const USERS = [
+        'admin' => 'secret123#'
+    ];
+
+    const AUTH_URL = '/api/login_check';
+    const AUTH_JSON = '
+        {
+            "username": "%s",
+            "password": "%s"
+        }
+    ';
+
+    /**
+     * @var AppFixtures
+     */
+    private $fixtures;
+    /**
+     * @var \Coduo\PHPMatcher\Matcher
+     */
+    private $matcher;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(
+        Request $request,
+        AppFixtures $fixtures,
+        EntityManagerInterface $em
+    )
+    {
+        parent::__construct($request);
+        $this->fixtures = $fixtures;
+        $this->matcher = (new SimpleFactory())->createMatcher();
+        $this->em = $em;
+    }
+
+    /**
+     * @Given I am authenticated as :user
+     */
+    public function iAmAuthenticatedAs($user)
+    {
+        $this->request->setHttpHeader('Content-Type', 'application/ld+json');
+        $this->request->send(
+            'POST',
+            $this->locatePath(self::AUTH_URL),
+            [],
+            [],
+            sprintf(self::AUTH_JSON, $user, self::USERS[$user])
+        );
+
+        $json = json_decode($this->request->getContent(), true);
+        // Make sure the token was returned
+        $this->assertTrue(isset($json['token']));
+
+        $token = $json['token'];
+
+        $this->request->setHttpheader(
+            'Authorization',
+            'Bearer '.$token
+        );
+    }
+
+    /**
+     * @BeforeScenario @createSchema
+     */
+    public function createSchema()
+    {
+        // Get entity metadata
+        $classes = $this->em->getMetadataFactory()->getAllMetadata();
+
+        // Drop and create schema
+        $schemaTool = new SchemaTool($this->em);
+        $schemaTool->dropSchema($classes);
+        $schemaTool->createSchema($classes);
+
+        // Load fixtures and execute
+        $purger = new ORMPurger($this->em);
+        $fixturesExecutor = new ORMExecutor($this->em, $purger);
+
+        $fixturesExecutor->execute([
+            $this->fixtures
+        ]);
+    }
+}
