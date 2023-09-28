e165e7b4ea546cdda1b958dc9f4bdcbfaa0a1b9d
alex <o.lototskyi@gmail.com>
2023-09-28 11:10:47 +0300

    ReactJS frontend: comments Load more button

---
+++ react/src/components/LoadMore.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export class LoadMore extends React.Component {
+
+
+    render() {
+        const {label, disabled, onClick} = this.props;
+
+        return (
+            <button className="btn btn-block btn-dark" onClick={onClick} disabled={disabled}>
+                {label}
+            </button>
+        );
+    }
+}
